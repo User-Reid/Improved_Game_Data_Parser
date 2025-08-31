98b8ac66fd1be6bbd54c177052f7207f24d1368b
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-30 19:39:06 -0500

    additional refactoring for plug and play dependency inversion and injection

--- Program.cs
+++ Program.cs
@@ -21,19 +21,33 @@ public class GameDataParserApp
   private readonly IUserInteractor _userInteractor;
   private readonly IGamesPrinter _gamesPrinter;
   private readonly IVideoGamesDeserializer _videoGamesDeserializer;
-  public GameDataParserApp(IUserInteractor userInteractor, IGamesPrinter gamesPrinter, IVideoGamesDeserializer videoGamesDeserializer)
+  private readonly IFileReader _fileReader;
+  public GameDataParserApp(IUserInteractor userInteractor, IGamesPrinter gamesPrinter, IVideoGamesDeserializer videoGamesDeserializer, IFileReader fileReader)
   {
     _userInteractor = userInteractor;
     _gamesPrinter = gamesPrinter;
     _videoGamesDeserializer = videoGamesDeserializer;
+    _fileReader = fileReader;
   }
   public void Run()
   {
     string fileName = _userInteractor.ReadValidFilePath();
-
-    var fileContents = File.ReadAllText(fileName);
+    var fileContents = _fileReader.Read(fileName);
     var videoGames = _videoGamesDeserializer.DeserializeFrom(fileName, fileContents);
     _gamesPrinter.Print(videoGames);
+  }
+
+  public interface IFileReader
+  {
+    string Read(string fileName);
+  }
+
+  public class LocalFileReader : IFileReader
+  {
+    public string Read(string fileName)
+    {
+      return File.ReadAllText(fileName);
+    }
   }
 
   public interface IVideoGamesDeserializer
